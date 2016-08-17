d011958f555ff7dd07f397b1a58211fb98f5c73f
Mahmudbek Odilov <mahmudbekstudio@gmail.com>
2016-08-17 13:04:33 +0500

    vendor and other files from template

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=trading',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+	'tablePrefix' => 'trade',
 ];
