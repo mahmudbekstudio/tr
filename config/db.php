4a70c20c6da8532dd31ade2caf7a3bca4915ff3a
Mahmudbek Odilov <mahmudbekstudio@gmail.com>
2016-10-03 14:48:20 +0500

    config compnay db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=trading-test',
+    'dsn' => 'mysql:host=localhost;dbname=' . (defined('COMPANY_DB') ? COMPANY_DB : 'trading-test'),
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
