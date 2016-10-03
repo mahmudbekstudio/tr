e808f2890f6d9fc9b7e2028bc8a2cab715c52ecc
Mahmudbek Odilov <mahmudbekstudio@gmail.com>
2016-10-03 15:15:33 +0500

    db settings for company

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=' . (defined('COMPANY_DB') ? COMPANY_DB : 'trading-test'),
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host='  . (defined('COMPANY_DBHOST') ? COMPANY_DBHOST : 'localhost') . ';dbname=' . (defined('COMPANY_DB') ? COMPANY_DB : 'trading-test'),
+    'username' => (defined('COMPANY_DBUSER') ? COMPANY_DBUSER : 'root'),
+    'password' => (defined('COMPANY_DBPASS') ? COMPANY_DBPASS : ''),
     'charset' => 'utf8',
-    'tablePrefix' => 'trade_'
+    'tablePrefix' => (defined('COMPANY_DBTABLEPREFIX') ? COMPANY_DBTABLEPREFIX : 'trade_')
 ];
